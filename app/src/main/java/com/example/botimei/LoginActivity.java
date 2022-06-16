7e6f7d7c418261b179ffa3d26f6781526ba2545d
YASH <yashrmehta108@gmail.com>
2022-06-17 00:08:43 +0530

    Login

--- app/src/main/java/com/example/botimei/LoginActivity.java
+++ app/src/main/java/com/example/botimei/LoginActivity.java
@@ -1,149 +1,101 @@
 package com.example.botimei;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
+import com.example.botimei.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.*;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // Initialize variables
-    SignInButton btSignIn;
-    GoogleSignInClient googleSignInClient;
-    FirebaseAuth firebaseAuth;
+    //declare objects
+    EditText email, password;
+//    LinearLayout logoImg;
+//    ImageButton btnIn;//signInButton
+//    Animation spls_fade;
+//    TextView signUpLink;
+
+    private FirebaseAuth mAuth;//declare fireBaseAuth object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        Button login;
+        login = findViewById(R.id.btnSignin);
 
-        // Assign variable
-        btSignIn=findViewById(R.id.bt_sign_in);
+//store value
+        //spls_fade = AnimationUtils.loadAnimation(this,R.anim.spls_fade);
+        email = (EditText) findViewById(R.id.signInUserName);
+        password = (EditText) findViewById(R.id.signInPassword);
 
-        // Initialize sign in options
-        // the client-id is copied form
-        // google-services.json file
-        GoogleSignInOptions googleSignInOptions=new GoogleSignInOptions.Builder(
-                GoogleSignInOptions.DEFAULT_SIGN_IN
-        ).requestIdToken("823082487377-pkrv1oq8p37dhqmpmbts47se3chv9jt5.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-
-        // Initialize sign in client
-        googleSignInClient= GoogleSignIn.getClient(LoginActivity.this
-                ,googleSignInOptions);
-
-        btSignIn.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Initialize sign in intent
-                Intent intent=googleSignInClient.getSignInIntent();
-                // Start activity for result
-                startActivityForResult(intent,100);
+                if (email.getText().toString().isEmpty()){
+                    email.setError("Username is Required");
+                }
+                if (password.getText().toString().isEmpty()){
+                    password.setError("Password is Required");
+                }
+                /*if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "All fields must be filled", Toast.LENGTH_LONG).show();*/
+                else if (password.getText().toString().length() < 8) {
+                    Toast.makeText(getApplicationContext(), "Password Must be Atleast 8 Characters Long", Toast.LENGTH_LONG).show();
+                } else {
+                    checkLogin(email.getText().toString(), password.getText().toString());
+                }
             }
         });
-
-        // Initialize firebase auth
-        firebaseAuth=FirebaseAuth.getInstance();
-        // Initialize firebase user
-        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
-        // Check condition
-        if(firebaseUser!=null)
-        {
-            // When user already sign in
-            // redirect to profile activity
-            startActivity(new Intent(LoginActivity.this,ProfileActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        }
     }
+    //login validation
+    private void checkLogin(String e, String p)
+    {
+        mAuth.signInWithEmailAndPassword(e, p)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("TAG","Hii there my value is"+task.getResult());
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Welcome"+email+"\nYou are logged in",Toast.LENGTH_LONG).show();
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Check condition
-        if(requestCode==100)
-        {
-            // When request code is equal to 100
-            // Initialize task
-            Task<GoogleSignInAccount> signInAccountTask=GoogleSignIn
-                    .getSignedInAccountFromIntent(data);
-
-            // check condition
-            if(signInAccountTask.isSuccessful())
-            {
-                // When google sign in successful
-                // Initialize string
-                String s="Google sign in successful";
-                // Display Toast
-                displayToast(s);
-                // Initialize sign in account
-                try {
-                    // Initialize sign in account
-                    GoogleSignInAccount googleSignInAccount=signInAccountTask
-                            .getResult(ApiException.class);
-                    // Check condition
-                    if(googleSignInAccount!=null)
-                    {
-                        // When sign in account is not equal to null
-                        // Initialize auth credential
-                        AuthCredential authCredential= GoogleAuthProvider
-                                .getCredential(googleSignInAccount.getIdToken()
-                                        ,null);
-                        // Check credential
-                        firebaseAuth.signInWithCredential(authCredential)
-                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        // Check condition
-                                        if(task.isSuccessful())
-                                        {
-                                            // When task is successful
-                                            // Redirect to profile activity
-                                            startActivity(new Intent(LoginActivity.this
-                                                    ,MainActivity.class)
-                                                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                                            // Display Toast
-                                            displayToast("Firebase authentication successful");
-                                        }
-                                        else
-                                        {
-                                            // When task is unsuccessful
-                                            // Display Toast
-                                            displayToast("Authentication Failed :"+task.getException()
-                                                    .getMessage());
-                                        }
-                                    }
-                                });
-
+                            Intent i;
+                            i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Failed\n Check your username or password",Toast.LENGTH_LONG).show();
+                        }
                     }
-                }
-                catch (ApiException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
+                });
     }
-
-    private void displayToast(String s) {
-        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
-    }
+    //redirect signup page event
+//    public void signUp(View view)
+//    {
+//        Intent signUpIntentObj = new Intent(getApplicationContext(),SignupActivity.class);
+//        startActivity(signUpIntentObj);
+//    }
 }
